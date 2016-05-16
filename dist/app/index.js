9a6214b9e2a7ae38961a6a25672e34d59470a796
yc355 <yifan.i.chen@gmail.com>
2016-05-15 23:14:18 -0400

    first commit

---
+++ dist/app/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var environment_1 = require('./environment');
+exports.environment = environment_1.environment;
+var tko_ng2_component_1 = require('./tko-ng2.component');
+exports.TkoNg2AppComponent = tko_ng2_component_1.TkoNg2AppComponent;
+//# sourceMappingURL=index.js.map
---
+++ dist/app/tko-ng2.component.js
@@ -0,0 +1,28 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var TkoNg2AppComponent = (function () {
+    function TkoNg2AppComponent() {
+        this.title = 'tko-ng2 works!';
+    }
+    TkoNg2AppComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'tko-ng2-app',
+            templateUrl: 'tko-ng2.component.html',
+            styleUrls: ['tko-ng2.component.css']
+        }), 
+        __metadata('design:paramtypes', [])
+    ], TkoNg2AppComponent);
+    return TkoNg2AppComponent;
+}());
+exports.TkoNg2AppComponent = TkoNg2AppComponent;
+//# sourceMappingURL=tko-ng2.component.js.map
